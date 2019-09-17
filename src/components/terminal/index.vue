ff946bb650116e9788bc85a4506d4345c8e84fd0
StitchCula <stitchgl@outlook.com>
2019-09-17 11:26:27 +0800

    原来hterm是内嵌iframe

--- src/components/terminal/index.vue
+++ src/components/terminal/index.vue
@@ -1,23 +1,63 @@
 <template>
-    <d2-container-frame src="/terminal" class="terminal" ></d2-container-frame>
+    <d2-container v-bind="$attrs">
+      <iframe
+        ref="terminal"
+        @load="onLoad"
+        class="d2-container-frame"
+        src="/terminal"
+        frameborder="0"></iframe>
+    </d2-container>
 </template>
 
 <script>
 
 export default {
   name: 'terminal',
+  props: {
+    rzUrl: String
+  },
   data () {
     return {}
   },
+  methods: {
+    onDrop (e) {
+      // if (!this.rzUrl) {
+      //   return
+      // }
+      e.stopPropagation()
+      e.preventDefault()
+      console.log(e.dataTransfer.files)
+    },
+    prevent (e) {
+      e.stopPropagation()
+      e.preventDefault()
+    },
+    onLoad (_) {
+      let body = this.$refs.terminal.contentDocument.body
+      let inner = body.querySelector('iframe')
+      if (inner) {
+        body = inner.contentDocument.body
+      }
+      body.addEventListener('dragleave', this.prevent, true)
+      body.addEventListener('dragover', this.prevent, true)
+      body.addEventListener('drop', this.onDrop, true)
+    }
+  },
   mounted () {
-    console.log('mounted')
+
   },
   destroyed () {
-    console.log('destroyed')
+    // console.log('destroyed')
   }
 }
 </script>
 
 <style scoped>
-
+.d2-container-frame {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+}
 </style>
--- src/views/demo/page2/index.vue
+++ src/views/demo/page2/index.vue
@@ -1,26 +1,51 @@
 <template>
   <d2-container>
-    <SplitPane :min-percent='20' :default-percent='30' split="vertical">
-      <template slot="paneR"><terminal></terminal></template>
-      <template slot="paneL"><div style="padding-right: 20px; height: 100%" v-resize:throttle.initial="resizeTableHeight">
-        <el-table :data="data.filter(it => !search || it.name.toLowerCase().includes(search.toLowerCase()))" style="width: 100%" :height="rTableHeight">
+    <SplitPane :min-percent='20' :default-percent='20' split="vertical">
+      <template slot="paneR">
+        <terminal rz-url="/upload"></terminal>
+      </template>
+
+      <template slot="paneL"><div style="padding-right: 20px; height: 100%" v-resize="resizeTableHeight">
+        <el-table :data="selectedData.filter(it => !search || it.name.toLowerCase().includes(search.toLowerCase()))" style="width: 100%" :height="rTableHeight" size="small">
           <el-table-column type="index" :index="indexMethod"></el-table-column>
-          <el-table-column prop="name">
+
+          <el-table-column prop="name" :show-overflow-tooltip="true">
             <template slot="header" slot-scope="/* eslint-disable vue/no-unused-vars */scope">
-              <el-input v-model="search" size="mini" placeholder="输入关键字搜索"/>
+              <el-input v-model="search" size="mini" placeholder="搜索"/>
             </template>
           </el-table-column>
+
           <el-table-column width="80" align="center">
             <template slot="header" slot-scope="/* eslint-disable vue/no-unused-vars */scope">
-              <el-button size="mini"><i class="el-icon-edit"></i></el-button>
+              <el-button size="mini" type="primary" plain @click="rTransferVisible = true"><d2-icon name="exchange"/></el-button>
             </template>
             <template slot-scope="/* eslint-disable vue/no-unused-vars */scope">
-              <el-button size="mini" @click="handleDelete(scope.$index, scope.row)"><i class="el-icon-edit"></i></el-button>
+              <el-button size="mini" type="danger" plain @click="handleDelete(scope.$index, scope.row)"><d2-icon name="key"/></el-button>
             </template>
           </el-table-column>
         </el-table>
       </div></template>
     </SplitPane>
+
+    <el-dialog title="批量选择" :visible.sync="rTransferVisible">
+      <div style="text-align: center">
+        <el-transfer
+          class="transfer-wrap"
+          filterable
+          :filter-method="filterMethod"
+          filter-placeholder="搜索"
+          :titles="['可用', '启用']"
+          :props="{ key: 'sid', label: 'name' }"
+          v-model="selectedSid"
+          :data="allData">
+        </el-transfer>
+      </div>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="rTransferVisible = false">取 消</el-button>
+        <el-button type="primary" @click="selectOK()">确 定</el-button>
+      </div>
+    </el-dialog>
+
   </d2-container>
 </template>
 
@@ -31,82 +56,44 @@ import resize from 'vue-resize-directive'
 export default {
   name: 'page2',
   data () {
+    const allData = []
+    const selectedSid = []
+    for (let i = 0; i < 10; i++) {
+      allData[i] = {
+        sid: i,
+        name: `测试1112 (192.168.1.${i})`
+      }
+      selectedSid.push(i)
+    }
     return {
-      data: [
-        {
-          date: '2016-05-02',
-          name: 'XXX路(192.168.5.64)',
-          address: '上海市普陀区金沙江路 1518 弄'
-        },
-        {
-          date: '2016-05-04',
-          name: 'XXX路(192.168.5.64)',
-          address: '上海市普陀区金沙江路 1517 弄'
-        },
-        {
-          date: '2016-05-01',
-          name: 'XXX路(192.168.5.64)',
-          address: '上海市普陀区金沙江路 1519 弄'
-        },
-        {
-          date: '2016-05-03',
-          name: 'XXX路(192.168.5.64)',
-          address: '上海市普陀区金沙江路 1516 弄'
-        },
-        {
-          date: '2016-05-03',
-          name: 'XXX路(192.168.5.64)',
-          address: '上海市普陀区金沙江路 1516 弄'
-        },
-        {
-          date: '2016-05-03',
-          name: 'XXX路(192.168.5.64)',
-          address: '上海市普陀区金沙江路 1516 弄'
-        },
-        {
-          date: '2016-05-03',
-          name: 'XXX路(192.168.5.64)',
-          address: '上海市普陀区金沙江路 1516 弄'
-        },
-        {
-          date: '2016-05-03',
-          name: 'XXX路(192.168.5.64)',
-          address: '上海市普陀区金沙江路 1516 弄'
-        },
-        {
-          date: '2016-05-03',
-          name: 'XXX路(192.168.5.64)',
-          address: '上海市普陀区金沙江路 1516 弄'
-        },
-        {
-          date: '2016-05-03',
-          name: 'XXX路(192.168.5.64)',
-          address: '上海市普陀区金沙江路 1516 弄'
-        },
-        {
-          date: '2016-05-03',
-          name: 'XXX路(192.168.5.64)',
-          address: '上海市普陀区金沙江路 1516 弄'
-        },
-        {
-          date: '2016-05-03',
-          name: 'XXX路(192.168.5.64)',
-          address: '上海市普陀区金沙江路 1516 弄'
-        }
-      ],
+      allData: allData,
+      selectedSid: selectedSid,
+      selectedData: JSON.parse(JSON.stringify(allData)),
       search: '',
-      rTableHeight: 300
+      rTableHeight: '0',
+      rTransferVisible: false
     }
   },
   methods: {
-    handleDelete (index, row) {
-      console.log(index, row)
-    },
-    indexMethod (index) {
-      return index * 2
+    indexMethod (i) {
+      return this.allData[i].sid
     },
     resizeTableHeight (it) {
       this.rTableHeight = window.getComputedStyle(it).height
+    },
+    filterMethod (query, item) {
+      return item
+    },
+    handleDelete (i, row) {
+      console.log(i, row)
+    },
+    selectOK () {
+      this.rTransferVisible = false
+      const selectedData = []
+      for (let sid of this.selectedSid) {
+        selectedData[sid] = this.allData[sid]
+      }
+      this.selectedData = selectedData
     }
   },
   components: {
@@ -117,3 +104,15 @@ export default {
   }
 }
 </script>
+
+<style>
+.transfer-wrap {
+  text-align: left;
+  display: inline-block;
+}
+
+.transfer-wrap > .el-transfer-panel {
+  width: 300px;
+}
+
+</style>
